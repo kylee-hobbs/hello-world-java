2026dea77fc28aa16482b9fbc60c04be510a6021
kylee-hobbs <kylee.hobbs318@yahoo.com>
2024-04-09 13:23:46 -0400

    Added and printed String
    I added a String called 'type1' and made it equal to "Hello world!", I
    then printed the String.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    String type1 = "Hello world!";
+    
+    System.out.println(type1);
   }
 }
